ce34b260ca306b471f0b476d762b73cd8743baa4
Jabin Washburn <jabinwashburn@gmail.com>
2020-07-24 10:36:11 -0700

    have most of the app working, my post isnt going onto the page figuing that out then testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import {Switch, Route} from 'react-router-dom'
+import Form from './Form'
+import OrderList from './OrderList'
+import axios from 'axios'
+
+const initialForm ={
+  name: '',
+  size: '',
+  olive: false, 
+  pep: false,
+  mushroom: false,
+  pine: false,
+  troll: false,
+  ins: '' 
+}
 
 const App = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+    .post('http://reqre.in/api/pizza', form)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => console.log(err))
+  }
+
+  const [form, setForm] = useState(initialForm)
+
+  const [order, setOrder] = useState([])
+
+
+  const handleChange = (e) => {
+    e.persist()
+    e.target.type === 'checkbox' ? 
+    setForm({...form, [e.target.name]: e.target.checked})
+    : setForm({...form, [e.target.name]: e.target.value})
+  }
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+
+      <Switch>
+
+        <Route path='/pizza'>
+          <OrderList order={order}/>
+        </Route>
+
+        <Route path='/'>
+          <Form form={form} handleChange={handleChange} handleSubmit={handleSubmit} />
+        </Route>
+
+      </Switch>
     </>
   );
 };
